95bf1a0798a82a6d5e2abe6cab192539b3661471
Serge Urakhchin <urakhchin@gmail.com>
2016-03-02 22:05:07 -0600

    Javascript functions, variables, eventts
    intro to basic programming topics with javascript

---
+++ JavascriptExamples.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Serge on 2/29/2016.
+ * Example of importing javascript into HTML file.
+ */
+function surprise() {
+    alert("Hello!");
+
+}
+surprise()
+
+var user_name; // create a variable
+user_name=prompt("What is your name"); //stores variable in prompt
+document.write(user_name); //displays variable in webpage
+
+/*
+ * javascript doesnt care about indentations.
+ * in javascript needs a semicolon at end of each line.
+ */
+
+/**variables
+ * javascript only has 1 type of number = var
+ * has string type same var name = "david";
+ * has boolean var condition1 = true;
+ * var condition2 = false;
+ * the type variable can be interchanged
+ */
+
+/*
+
+count = count + 1
+count ++
+
+count = count + 10
+count += 10
+
+count = count - 1
+count --
+
+hello = hello + "1"
+hello +=
+
+*/
+
+/* from one type to another:
+parseInt() converts to an interger
+parseFloat() converts to a float with decimal
+String() can put a number and will convert to text
+ */
+
